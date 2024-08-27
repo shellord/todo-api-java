f06b846b35fd06ed32df4bb531d31ce6d849ff8e
Saheen Shoukath <saheenshoukath@gmail.com>
2024-08-27 15:21:43 +0530

    feat: add project

---
+++ src/main/java/org/shellord/todo/controllers/TodoController.java
@@ -0,0 +1,11 @@
+package org.shellord.todo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+}
